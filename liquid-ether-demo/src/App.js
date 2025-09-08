11954ad6e4d94deec719ef94a377684d2eb14eae
Hridoy1088 <hridoyhossain200400@gmail.com>
2025-09-08 15:24:23 +0600

    Initial commit

---
+++ liquid-ether-demo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LiquidEther from './LiquidEther';
+import './LiquidEther.css';
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <LiquidEther />
+    </div>
+  );
+}
+
+export default App;
